d503f87eca99742ca0c3d8c20b3984cc6116ad92
Col D <60669354+tward90@users.noreply.github.com>
2020-09-08 22:07:13 -0500

    Additional Table tweaking. playing with styling

--- Table Files/table.js
+++ Table Files/table.js
@@ -1,4 +1,14 @@
 
+const djiStocks = ['AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'GS', 'HD', 'HON', 'IBM', 'INTC', 'JNJ', 'KO', 'JPM', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 
+                    'PG', 'TRV', 'UNH', 'CRM', 'VZ', 'V', 'WBA', 'WMT', 'DIS', 'DOW']
+
+
+                
+// For Each stock in djiStocks, find all instances of it and locate the max date.
+//
+
+console.log(djiStocks);
+
 $(document).ready( function () {
-    $('#table_id').DataTable();
+    $('#stock_data').DataTable();
 } );
