63f5800056dab622fb65d32495896fefb9bbb8a5
Col D <60669354+tward90@users.noreply.github.com>
2020-09-11 19:50:45 -0500

    Test Deployment for Table

--- Table Files/table.js
+++ Table Files/table.js
@@ -1,11 +1,4 @@
-
-// const djiStocks = ['AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'GS', 'HD', 'HON', 'IBM', 'INTC', 'JNJ', 'KO', 'JPM', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 
-//                     'PG', 'TRV', 'UNH', 'CRM', 'VZ', 'V', 'WBA', 'WMT', 'DIS', 'DOW']
-
-// const jsonLink = 'http://127.0.0.1:5000/jsonified/'
-
 const jsonLink = 'https://pet-pal88.herokuapp.com/max_date'
-
 
 const tbody = d3.select("tbody");
 
@@ -13,7 +6,7 @@ d3.json(jsonLink).then(djiData => {
 
     const data = Object.values(djiData)
 
-    // console.log(data)
+    console.log(data)
     
     data.forEach(stock_day => {
 
@@ -41,30 +34,3 @@ d3.json(jsonLink).then(djiData => {
     })
 
 })
-
-
-/*
-    function buildTable(data) {
-        // First, clear out any existing data
-        tbody.html("");
-      
-        // Next, loop through each object in the data
-        // and append a row and cells for each value in the row
-        data.forEach((dataRow) => {
-          // Append a row to the table body
-          const row = tbody.append("tr");
-      
-          // Loop through each field in the dataRow and add
-          // each value as a table cell (td)
-          Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-              cell.text(val);
-            }
-          );
-        });
-      }
-      buildTable(djiData);
-*/
-               
-// For Each stock in djiStocks, find all instances of it and locate the max date.
-//
