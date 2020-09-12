33ca28e8928e20447d686ef9bfcf015604d0f66f
Col D <60669354+tward90@users.noreply.github.com>
2020-09-12 14:23:10 -0500

    Working HTML and style.css. Still need to address sizing of sunburst.

---
+++ static/js/table.js
@@ -0,0 +1,41 @@
+// const jsonLink = 'https://pet-pal88.herokuapp.com/max_date'
+
+const jsonLink = '/max_date'
+
+const tbody = d3.select("tbody");
+
+d3.json(jsonLink)
+// d3.json(jsonLink, {headers: new Headers({ RequestMode : "no-cors"})})
+    // .header({'Access-Control-Allow-Origin': 'https://pet-pal88.herokuapp.com/max_date'})
+    .then(djiData => {
+
+    const data = Object.values(djiData)
+
+    console.log(data)
+    
+    data.forEach(stock_day => {
+
+        let row = tbody.append("tr");
+        let date = new Date(stock_day['date_close']);
+        let day = date.getDate() + 1
+        let month = date.getMonth() + 1
+        let year = date.getFullYear()
+        row.append('td').text(year + "-" + month + "-" + day);
+        row.append('td').text(stock_day['company']);
+        row.append('td').text(stock_day['ticker']);
+        row.append('td').text(stock_day['open']);
+        row.append('td').text(stock_day['low']);
+        row.append('td').text(stock_day['high']);
+        row.append('td').text(stock_day['close']);
+        row.append('td').text(stock_day['volume']);
+
+        // Object.entries(day).forEach(([key, value]) => {
+        //         row.append("td").text(value['']);
+        // });
+    })
+
+    $(document).ready( function () {
+      $('#stock_data').DataTable();
+    })
+
+})
