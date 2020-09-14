0829c69bf03858b2a99a4f3fa5f1a7ce5b44d3bb
Col D <60669354+tward90@users.noreply.github.com>
2020-09-14 18:29:41 -0500

    Adding ticker and table js files to master

---
+++ static/js/table.js
@@ -0,0 +1,35 @@
+
+
+const jsonLink = "/max_date"
+
+const tbody = d3.select("tbody");
+
+d3.json(jsonLink).then(djiData => {
+
+    // console.log(djiData);
+
+    const data = Object.values(djiData)
+
+    data.forEach(stock_day => {
+
+        let row = tbody.append("tr");
+        let date = new Date(stock_day['date_close']);
+        let day = date.getDate() + 1
+        let month = date.getMonth() + 1
+        let year = date.getFullYear()
+        row.append('td').text(year + "-" + month + "-" + day);
+        row.append('td').text(stock_day['company']);
+        row.append('td').text(stock_day['ticker']);
+        row.append('td').text(stock_day['open']);
+        row.append('td').text(stock_day['low']);
+        row.append('td').text(stock_day['high']);
+        row.append('td').text(stock_day['close']);
+        row.append('td').text(stock_day['volume']);
+
+    })
+
+    $(document).ready( function () {
+      $('#stock_data').DataTable();
+    })
+
+})
